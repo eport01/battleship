abeddc98d135a08cce2a16dd19ebaf5ee1a0a456
Emily Port <emilylport@gmail.com>
2022-08-30 07:47:37 -0600

    Initial commit

---
+++ spec/ship_spec.rb
@@ -0,0 +1,34 @@
+require 'rspec'
+require './lib/ship.rb'
+
+RSpec.describe Ship do 
+
+it 'initializes' do 
+  
+  cruiser = Ship.new("Steve", 3)
+
+  expect(cruiser.name).to eq("Steve")
+  expect(cruiser.length).to eq(3)
+
+
+end 
+
+# it 'ship can be sunk' do 
+
+#   cruiser = Ship.new("Cruiser", 3)
+#   expect(cruiser.health).to eq(3)
+#   expect(cruiser.sunk?).to eq(false)
+#   cruiser.hit
+#   expect(cruiser.health).to eq(2)
+#   cruiser.hit 
+#   expect(cruiser.health).to eq(1)
+#   expect(cruiser.sunk?).to eq(false)
+#   cruiser.hit
+#   expect(cruiser.sunk?).to eq(true)
+
+# end 
+
+
+
+
+end 
