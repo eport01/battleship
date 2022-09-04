7669b09aaf8309df5a67cdc1fe621914af1ad0e8
Emily Port <emilylport@gmail.com>
2022-09-04 10:40:34 -0600

    add print welcome message

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,18 @@
 class Game 
   def initialize
+    @welcome_message = "Welcome to BATTLESHIP
+    Enter p to play. Enter q to quit."
+
 
   end 
+  def welcome_message
+   "Welcome to BATTLESHIP
+    Enter p to play. Enter q to quit."
+  end 
+
+
+
+
 
 
 end 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,8 +7,15 @@ require './lib/game'
 
 RSpec.describe Game do 
   it 'initializes' do 
-    game = Game.new 
+    game = Game.new
     expect(game).to be_an_instance_of(Game)
+
+  end 
+
+  it 'prints welcome message' do 
+    game = Game.new
+    expect(game).to eq("Welcome to BATTLESHIP
+      Enter p to play. Enter q to quit.")
 
   end 
 
