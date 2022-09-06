141cbfa0d52c0f648deb196e7cec58ff5dcd58ad
Lacey Weaver <jlweave89@gmail.com>
2022-09-06 17:47:27 -0600

    Add test to check has lost

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,11 +6,11 @@ require './lib/game'
 require './lib/player'
 
 
-RSpec.describe Game do 
-  it 'initializes' do 
+RSpec.describe Game do
+  it 'initializes' do
     user = Player.new('User')
     computer = Player.new('Computer')
     game = Game.new(computer, user)
     expect(game).to be_an_instance_of(Game)
-  end 
+  end
 end
